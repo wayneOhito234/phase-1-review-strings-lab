942f409d4769ad45b95d7764ace578b87a267531
+ <ohitowayneaol@gmail.com>
2025-06-19 21:32:45 +0300

    2nd Update

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Step 1: Define the user
+const currentUser = 'Grace Hopper';
+
+// Step 2: Greeting with the user's name (with interpolation)
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Step 3: Excited greeting (uppercase version)
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Step 4: Short greeting with only the first letter of the user's name
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
